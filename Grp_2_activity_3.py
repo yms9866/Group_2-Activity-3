842efa35f6961bfc1143e7506687a7007077bad2
Yosef <yms9866@rit.edu>
2024-04-10 18:16:42 +0400

    Version_3

--- Grp_2_activity_3.py
+++ Grp_2_activity_3.py
@@ -57,6 +57,46 @@ def one_column_load(file_path,column):
                 row+=1
             return empty_cells 
 
+def number_of_column(file_path):
+    """Returns the number of columns that the csv file has."""
+    with open(file_path,'r') as file:
+        csv_r = csv.reader(file)
+        for i in csv_r:
+            return len(i)
+
+
+def name_of_column(file_path):
+        """Returns a list of the names of columns in the csv file."""
+        with open(file_path,'r') as file:
+            csv_r = csv.reader(file)
+            for i in csv_r:
+                return i
+
+def return_list(file_name,column):
+    """Returns a list of the values under a specified column."""
+    with open(file_name,'r') as file:
+        csv_r = csv.reader(file)
+        next(csv_r)
+        lst=[]
+        for i in csv_r:
+            try:
+                lst.append(float(i[column-1].strip()))
+            except ValueError:
+                lst.append(i[column-1].strip())
+        return lst
+    
+
+def empty(lst):
+    """Returns a list of indices whereby there is no value associated with the indices. """
+    j=0
+    empty_cell=[]
+    for i in lst:
+        if lst[j] == '':
+            empty_cell.append(j)
+        j+=1
+    return empty_cell
+
+
 
 def min(lst):
     """Returns the minimum value of a list."""
@@ -151,3 +191,144 @@ def visualizeData(lst):
         
         print(strg)
 
+def main():
+        print("""=========================================================================
+Welcome to Data Analysis CLI
+=========================================================================
+Program stages:
+1. Load Data 
+2. Clean and prepare data
+3. Analyse Data
+4. Visualize Data"""
+) 
+        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+        print('Stage 1: Load Data ')
+        while True:
+            try:
+                file = input("Enter your file path: ")
+                number_of_columns=number_of_column(file)
+                name = name_of_column(file)
+                input('Loading... Press enter')
+                load_data(file,1,number_of_columns)
+                input('Your file is loaded successfully. Press Enter to continue ')
+                print('Your file contains',number_of_columns,'columns named as: ')
+
+                column_chices='' # gives choices of names of columns as separated by /
+                for i in name:
+                    column_chices+=i+'/'
+                while True:    
+                    n=1
+                    for i in range(number_of_columns):
+                        print(str(n)+'.',name[i].strip())
+                        n+=1
+
+                    column=input('Which column do you want to analyse? \nChoose '+column_chices+' ').capitalize()
+                    
+                    column_number = 0  # changing the name of column given by the user to number to know which column number is the given name.
+                    for i in name:
+                        column_number+=1
+                        if column == i.strip():
+                            break
+
+                    if column== 'Price' or column== 'Units sold':
+                        print('Here is your chosen colomn. ')
+                        empty_rows = one_column_load(file,column_number)
+                        lst=return_list(file,column_number)
+                        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+                        input('Press Enter to go to the next stage. ')
+                        print('Stage 2: Cleaning and Preparing Data')
+                        print('\nHere is the list\n',lst,'\n')
+                        if empty_rows!=[]:
+                            print('You have empty cell at: ')
+                            for i in empty_rows:
+                                print('\t',i)
+                            while True:
+                                if len(empty_rows) == 1:
+                                    replace=input("""What do you want to do about this cell? Do you want to replace it with
+        ************************************************************************
+        1. Average of the list.
+        2. Minimum value of the list 
+        3. Maximum value of the list
+        ************************************************************************
+        Choose 1/2/3  """)
+                                else:
+                                    replace=input("""What do you want to do about these cells? Do you want to replace them with
+        ************************************************************************
+        1. Average of the list.
+        2. Minimum value of the list 
+        3. Maximum value of the list
+        ************************************************************************
+        Choose 1/2/3  """)
+                                if replace == '1':
+                                        avg = average(lst)
+                                        print('All empty values replaced with average value which is',avg)
+                                        lst2=replace_avg(lst)
+                                elif replace == '2':
+                                        mini = min(lst)
+                                        print('All empty values replaced with minimum value which is',mini)
+                                        lst2=replace_min(lst)
+                                elif replace == '3':
+                                        maxi = average(lst)
+                                        print('All empty values replaced with minimum value which is',maxi)
+                                        lst2=replace_max(lst)
+                                else:
+                                    print('Input Error. Try again. ')
+                                    continue
+                                break
+                            
+                            print('\nHere is the replaced list\n',lst2,'\n')
+                            print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+                            print('Stage3: Analysing Data')
+                            
+                            while True:
+                                sort_choice = input("""Do you want to sort the list in: 
+        1. Ascending order
+        2. Decending order
+        Choose 1/2 """)
+                                if sort_choice == '1':
+                                    sorted=sort_ascending(lst2)
+                                elif sort_choice == '2':
+                                    sorted=sort_descending(lst2)
+                                else:
+                                    print('Input Error. Try again.')
+                                    continue
+                                break
+                            print('\nHere is your sorted list \n',sorted,'\n')
+                            print('************************************************************************')
+                            print('Stage 4: Visualizing Data')
+                            print('Column:',column)
+                            print('Legend: each * represents 5 units')
+                            visualizeData(sorted)
+                            print('Visualization Completed.')
+                            print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+                        elif column == 'Branch' or column=='Product':
+                            print('You have chosen a non-numeric column. Try again !')
+                            continue
+                        else:
+                            print('Input Error. Try again!')
+                            continue
+                        repeat = input('Do you want to conntinue analyzing the other numerical columns? yes/no').lower()
+                        if repeat=='yes':
+                            continue
+                        elif repeat == 'no':
+                            break
+                    break
+                restart=input('Press 1 to go back to the first stage or 0 to exit. ')
+                if restart == '1':
+                    continue
+                elif restart == '0':
+                    print('The proogram is closed.')
+                    print('Thank you and Goodbye!')
+                    break
+                else:
+                    print('Input Error. Try again!')
+                    continue  
+            except FileNotFoundError:
+                print('File does not exist.')
+            except Exception:
+                print('Something went wrong. Please try again')
+
+
+            
+if __name__ == "__main__": 
+    main()
